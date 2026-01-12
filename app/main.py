84030604b658b935095e5b0549d166139fdfed2f
gitmaciejpiotr <97894371+gitmaciejpiotr@users.noreply.github.com>
2026-01-12 17:44:39 +0100

    fixed bugs

--- app/main.py
+++ app/main.py
@@ -3,56 +3,54 @@ from __future__ import annotations
 
 class Distance:
     # Write your code here
-    def __init__(self, distance: float) -> None:
-        self.distance = distance
+    def __init__(self, km: float) -> None:
+        self.km = km
 
     def __str__(self) -> str:
-        return f"Distance: {self.distance} kilometers"
+        return f"Distance: {self.km} kilometers"
 
     def __repr__(self) -> str:
-        return f"Distance(km={self.distance})"
+        return f"Distance(km={self.km})"
 
     def __add__(self,
                 other_distance: Distance | int | float) -> Distance:
         if not isinstance(other_distance, Distance):
             return Distance(
-                self.distance + other_distance
+                self.km + other_distance
             )
         return Distance(
-            self.distance + other_distance.distance
+            self.km + other_distance.km
         )
 
     def __iadd__(self,
                  other_distance: Distance | int | float) -> Distance:
         if not isinstance(other_distance, Distance):
-            return Distance(
-                self.distance + other_distance
-            )
-        return Distance(
-            self.distance + other_distance.distance
-        )
+            self.km += other_distance
+            return self
+        self.km += other_distance
+        return self
 
     def __mul__(self, other: int | float) -> object:
         return Distance(
-            self.distance * other
+            self.km * other
         )
 
     def __truediv__(self, other: int | float) -> object:
         return Distance(
-            round(self.distance / other, 2)
+            round(self.km / other, 2)
         )
 
     def __lt__(self, other: int | float) -> bool:
-        return self < other
+        return self.km < other
 
     def __gt__(self, other: int | float) -> bool:
-        return self > other
+        return self.km > other
 
     def __eq__(self, other: int | float) -> bool:
-        return self == other
+        return self.km == other
 
     def __le__(self, other: int) -> bool:
-        return self <= other
+        return self.km <= other
 
     def __ge__(self, other: int) -> bool:
-        return self >= other
+        return self.km >= other
